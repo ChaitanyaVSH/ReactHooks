5e7bf1cd49fb8d745e049a1e88504d5e6359e191
Chaitanya <chaitu.vsh@gmail.com>
2020-10-30 23:33:43 +0530

    React Hooks Day 4: useEffect and fetching individual data based on id best practices

--- src/components/useEffectHook/FetchingData/DataFetching.js
+++ src/components/useEffectHook/FetchingData/DataFetching.js
@@ -3,25 +3,41 @@ import axios from "axios";
 import "./posts.css";
 
 function DataFetching() {
-  const [posts, getPosts] = useState([]);
+  const [post, getPost] = useState({});
+  const [id, setId] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((response) => {
         console.log(response);
-        getPosts(response.data);
+        getPost(response.data);
       })
       .catch(() => {});
-  }, []);
+  }, [id]);
+
+  const handleChange = (e) => {
+    setId(e.target.value);
+  };
+
   return (
     <div>
       <h1>Posts</h1>
-      {posts.map((post) => (
+      <input
+        type="text"
+        name="postid"
+        value={id}
+        placeholder="Enter id"
+        onChange={handleChange}
+      />
+      <br />
+      <hr />
+      <h2>{post.title}</h2>
+      {/* {posts.map((post) => (
         <h2 key={post.id} className="postcontainer">
           {post.title}
         </h2>
-      ))}
+      ))} */}
     </div>
   );
 }
