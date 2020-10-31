a9cc264129198224bbd171cca84c20e874558c90
Chaitanya <chaitu.vsh@gmail.com>
2020-10-31 19:03:32 +0530

    React Hooks Day 4: useContext hook, importing context and consuming it using the hooks

--- src/components/useContextHook/ComponentE.js
+++ src/components/useContextHook/ComponentE.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { userContext } from "../../App";
 
 function ComponentE() {
+  const user = useContext(userContext);
   return (
     <div className="container">
       <h1>This is the Component E</h1>
+      <strong>Steps to consume context using hook</strong>
+      <ol>
+        <li>Import useContext</li>
+        <li>Import the necessary Contexts</li>
+        <li>store the context in a local varible</li>
+      </ol>
+      {user}
     </div>
   );
 }
